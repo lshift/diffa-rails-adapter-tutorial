2b0fb74b187e4fd6fe14c67a0111dd0bb1b2daee
Ceri Storey <ceri@lshift.net>
2012-07-16 16:05:53 +0100

    Rename grid models spec.

--- spec/unit/diffa/grid_models_spec.rb
+++ spec/unit/diffa/grid_models_spec.rb
@@ -5,7 +5,7 @@ describe Diffa::GridModel do
   let (:model) { Diffa::GridModel.new(initial_data) }
 
 
-  describe "update" do
+  describe "#update" do
     it "updates the initial_data" do
       new_value = [{"id" => "1", "version" => 2}]
       expect { model.update(new_value) }.to change { model.data }.
