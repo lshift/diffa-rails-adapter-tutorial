df7048653f79559fb15eb93c698a7a7e41a1aaf2
Ceri Storey <ceri@lshift.net>
2012-07-16 15:20:46 +0100

    Make fewer assumptions about the data in the system tests.

--- spec/system/skeleton_spec.rb
+++ spec/system/skeleton_spec.rb
@@ -12,17 +12,18 @@ Capybara.javascript_driver = :poltergeist
 describe "Application skeleton", :js => true do
   let (:app) { Diffa::ParticipantDemo.new }
   let (:driver) { Diffa::Test::AppDriver.new(app.rackapp) }
+  let (:scan_client) { Diffa::Test::ScanClient.new(app.rackapp) }
 
   it "Can add a row and save it to the server" do
     driver.show_grid
     new_row = ["Some identifier", "A dummy version"]
     headers = ["id", "version"]
 
-    driver.grid.should include(["sample-id0", "sample-version0"])
     expect {
       driver.add_entity(new_row)
       driver.save_to_server
-    }.to change { app.data.size }.from(1).to(2)
+    }.to change { app.data.size }.by(1)
+
     app.data.last.should == Hash[*headers.zip(new_row).flatten]
   end
 end
