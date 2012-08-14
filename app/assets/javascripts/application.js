4621a4c7add41c5e8dd9b4a8fee2cf82842ca44c
Ceri Storey <ceri@lshift.net>
2012-08-14 17:47:40 +0100

    Assume that the containing page will include jQuery.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
+//_ require jquery
+//_ require jquery_ujs
 //= require es5-shim.js
 //= require underscore.js
 //= require backbone.js
