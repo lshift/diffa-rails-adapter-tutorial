aadeb4bab59acd8f9c7a1a946002ff84f54d25b2
Ceri Storey <ceri@lshift.net>
2012-08-29 11:51:43 +0100

    Ensure database orders according to id and type_code for the risks view.

--- db/migrate/20120823145100_risk_aggregates.rb
+++ db/migrate/20120823145100_risk_aggregates.rb
@@ -1,6 +1,6 @@
 class RiskAggregates < ActiveRecord::Migration
   def up
-    execute %q{create view risks_view (id, trade_date, type_code, version, user) as select trade_id, trade_date, 'F', coalesce(version, ''), user_id from futures union select trade_id, trade_date, 'O', coalesce(version, ''), user_id from options order by id}
+    execute %q{create view risks_view (id, trade_date, type_code, version, user) as select trade_id, trade_date, 'F', coalesce(version, ''), user_id from futures union select trade_id, trade_date, 'O', coalesce(version, ''), user_id from options order by id, type_code}
 
     execute "create view risks_trade_date_yearly (user, trade_date, version) as (select user, date_format(trade_date, '%Y'), md5(group_concat(version order by id asc separator '')) from risks_view group by user, date_format(trade_date, '%Y'))"
     execute "create view risks_trade_date_monthly (user, trade_date, version) as (select user, date_format(trade_date, '%Y-%m'), md5(group_concat(version order by id asc separator '')) from risks_view group by user, date_format(trade_date, '%Y-%m'))"
