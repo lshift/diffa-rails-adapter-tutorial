e125139019de92779f6ab8dcc11c99b5bbdc8b0c
Ceri Storey <ceri@lshift.net>
2012-09-03 11:10:55 +0100

    Expires Month → Month.

--- app/assets/javascripts/grids.js
+++ app/assets/javascripts/grids.js
@@ -344,7 +344,7 @@ Diffa.GridView.CheckmarkFormatter = function CheckmarkFormatter(row, cell, value
                 editor: Slickback.NumberCellEditor},
             {id: "price", name: "Price", field: "price",
                 editor: Slickback.NumberCellEditor, precision: 2},
-            {id: "month", name: "Expires Month", field: "month", width: dateWidth,
+            {id: "month", name: "Month", field: "month", width: dateWidth,
                  editor: Slickback.NumberCellEditor},
             {id: "year", name: "Year", field: "year", width: dateWidth,
                  editor: Slickback.NumberCellEditor},
