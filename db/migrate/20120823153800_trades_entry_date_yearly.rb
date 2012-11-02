bec68b2ae461c9af44927e52b6b806ca2f0bde51
Anthony Harris <anthony@lshift.net>
2012-11-02 16:23:34 +0000

    Fixed aggregation views to have date time so that range queries work as expected.

--- db/migrate/20120823153800_trades_entry_date_yearly.rb
+++ db/migrate/20120823153800_trades_entry_date_yearly.rb
@@ -1,6 +1,6 @@
 class TradesEntryDateYearly < ActiveRecord::Migration
   def up
-    execute "create view trades_entry_date_yearly (user, entry_date, version) as (select user, date_format(entry_date, '%Y'), md5(group_concat(version order by id asc separator '')) from trades_view group by user, date_format(entry_date, '%Y'))"
+    execute "create view trades_entry_date_yearly (user, entry_date, version) as (select user, date(date_format(entry_date, '%Y-01-01')), md5(group_concat(version order by id asc separator '')) from trades_view group by user, date(date_format(entry_date, '%Y-01-01')))"
   end
 
   def down
--- db/migrate/20120823153801_trades_entry_date_monthly.rb
+++ db/migrate/20120823153801_trades_entry_date_monthly.rb
@@ -1,6 +1,6 @@
 class TradesEntryDateMonthly < ActiveRecord::Migration
   def up
-    execute "create view trades_entry_date_monthly (user, entry_date, version) as (select user, date_format(entry_date, '%Y-%m'), md5(group_concat(version order by id asc separator '')) from trades_view group by user, date_format(entry_date, '%Y-%m'))"
+    execute "create view trades_entry_date_monthly (user, entry_date, version) as (select user, date(date_format(entry_date, '%Y-%m-01')), md5(group_concat(version order by id asc separator '')) from trades_view group by user, date(date_format(entry_date, '%Y-%m-01')))"
   end
 
   def down
